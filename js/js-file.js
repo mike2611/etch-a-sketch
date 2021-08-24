c6ba359a99720d06b2e51add9562eb5ae0e91756
mike2611 <mpuentes2611@gmail.com>
2021-08-23 22:49:56 -0500

    Add linters CSS/HTML/JS and fix linter errors

--- js/js-file.js
+++ js/js-file.js
@@ -1,83 +1,78 @@
-  let board = document.getElementById("boardBorder");
+const board = document.getElementById('boardBorder');
 
-  function createBoard() {
-    for (let i = 0; i < 256; i++) {
-      let square = document.createElement("div");
-      square.classList.add("square");
-      square.onmouseover = function(x){
-        x.target.style.background = "tomato";
-      }
-      board.appendChild(square);
-    }
+function createBoard() {
+  for (let i = 0; i < 256; i += 1) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.onmouseover = (x) => {
+      x.target.style.background = 'tomato';
+    };
+    board.appendChild(square);
   }
+}
 
-  function cleanBoard() { 
-    let square = document.getElementsByClassName("square");
-    for ( let i = 0; i < square.length; i++){
-      square[i].style.background = setPrimary();
-    }
+function setPrimary() {
+  const primaryColor = document.getElementById('primary-color').value;
+  return primaryColor;
+}
+
+function setSecondary() {
+  const secondaryColor = document.getElementById('secondary-color').value;
+  return secondaryColor;
+}
+
+function cleanBoard() {
+  const square = document.getElementsByClassName('square');
+  for (let i = 0; i < square.length; i += 1) {
+    square[i].style.background = setPrimary();
   }
+}
 
-  function setSize() {
-    board.innerHTML = '';
-    let valueSize = document.getElementById("slider").value;
-    let sizeLength = 480/valueSize;
-    for (let i = 0; i < sizeLength * sizeLength; i++) {
-      let square = document.createElement("div");
-      square.classList.add("square");
-      square.style.width = valueSize + "px";
-      square.style.height = valueSize + "px";
-      square.onmouseover = function(x){
-        x.target.style.background = setSecondary();
-      }
-      board.appendChild(square);
-    }
-    cleanBoard();
-  }
-
-  function setPrimary() {
-    let primaryColor = document.getElementById("primary-color").value;
-    return primaryColor;
-  }
-
-  function setSecondary() {
-    let secondaryColor = document.getElementById("secondary-color").value;
-    return secondaryColor;
-  }
-
-
-  function setColor() {
-    setSize();
-    cleanBoard();
-  }
-
-  function reset() {
-    cleanBoard();
-  }
-
-  function resize() {
-    setSize();
-  }
-
- function rainbow() {
-
+function setSize() {
   board.innerHTML = '';
-  let valueSize = document.getElementById("slider").value;
-  let sizeLength = 480/valueSize;
-  for (let i = 0; i < sizeLength * sizeLength; i++) {
-    let square = document.createElement("div");
-    square.classList.add("square");
-    square.style.width = valueSize + "px";
-    square.style.height = valueSize + "px";
-    square.onmouseover = function(x){
-      x.target.style.background = 
-      `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
-    }
+  const valueSize = document.getElementById('slider').value;
+  const sizeLength = 480 / valueSize;
+  for (let i = 0; i < sizeLength * sizeLength; i += 1) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.width = `${valueSize}px`;
+    square.style.height = `${valueSize}px`;
+    square.onmouseover = (x) => {
+      x.target.style.background = setSecondary();
+    };
     board.appendChild(square);
   }
   cleanBoard();
+}
 
- }
+function setColor() { // eslint-disable-line
+  setSize();
+  cleanBoard();
+}
 
+function reset() { // eslint-disable-line
+  cleanBoard();
+}
 
-  window.onload = createBoard();
+function resize() { // eslint-disable-line
+  setSize();
+}
+
+function rainbow() { // eslint-disable-line
+  board.innerHTML = '';
+  const valueSize = document.getElementById('slider').value;
+  const sizeLength = 480 / valueSize;
+  for (let i = 0; i < sizeLength * sizeLength; i += 1) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.width = `${valueSize}px`;
+    square.style.height = `${valueSize}px`;
+    square.onmouseover = (x) => {
+      x.target.style.background = `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
+    };
+    board.appendChild(square);
+  }
+  cleanBoard();
+}
+
+window.onload = createBoard();
